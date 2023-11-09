86fd034e4f54a95895d61ca078cadb035cc17ce3
zakaria mohamed <zakmi.moha@gmail.com>
2023-11-09 06:56:18 +0300

    Done lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
-import React from "react";
 
-function Home() {
-  return <div>{/*{code here}*/}</div>;
-}
+import React from 'react';
+
+const Home = () => {
+  return <h1>Home Page</h1>;
+};
 
 export default Home;
